330fe20f8c5e49ee6faa357d6e3fdb6407eb8f17
Ewa Gauze <gauze.ewa@gmail.com>
2020-07-21 17:53:36 +0200

    dodanie 2 klas

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        MathQuiz mathsQuiz = new MathQuiz();
+        mathsQuiz.question1();
+        mathsQuiz.question2();
+        mathsQuiz.question3();
+        mathsQuiz.printResult();
+    }
+}
